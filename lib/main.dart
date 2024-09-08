d8d3ea43633c429665b7400e15401abdf5086c79
DESKTOP-T4N7NAE\Jarfilled <manihemedani@gmail.com>
2024-09-08 13:53:12 +0330

    l

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,9 @@
-import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flame/game.dart';
 
-import 'ember_quest.dart';
-import 'overlays/game_over.dart';
-import 'overlays/main_menu.dart';
+class SpaceShooterGame extends FlameGame {
+}
 
 void main() {
-  runApp(
-    GameWidget<EmberQuestGame>.controlled(
-      gameFactory: EmberQuestGame.new,
-      overlayBuilderMap: {
-        'MainMenu': (_, game) => MainMenu(game: game),
-        'GameOver': (_, game) => GameOver(game: game),
-      },
-      initialActiveOverlays: const ['MainMenu'],
-    ),
-  );
+  runApp(GameWidget(game: SpaceShooterGame()));
 }
